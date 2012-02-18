0ad7325f017e3a74536bf8bfd1c5720a2482ec00
Ben <salzig+github@gmail.com>
2012-02-18 01:41:33 +0100

    basic capistrano, unicorn configuration

---
+++ config/config.rb
@@ -0,0 +1,10 @@
+def project
+  {
+    name: "evulgo",
+    path: "/home/evulgo/www",
+    user: "evulgo",
+    group: 'staff',
+    git: "git@codeplane.com:you/my_site.git",
+    server: "88.198.106.201"
+  }
+end
