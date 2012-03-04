612fd03d98e97be02c5ba1111d60e2f93dd50b6a
Ben <salzig+github@gmail.com>
2012-03-04 20:01:25 +0100

    basic attribute check

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Article do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Article.new }
+  it "should have attribute content" do
+    should respond_to(:content)
+    should respond_to(:content=)
+  end
 end
