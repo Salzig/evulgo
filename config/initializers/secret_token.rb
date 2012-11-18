100609a0ef776e7ca73bde83b0b183602c353e61
Ben Rexin <salzig+github@gmail.com>
2012-11-18 01:11:24 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evulgo::Application.config.secret_token = '352b2fe003e592b4711f38999ac1e2653554f33b5eb5358fe80221153b1e3349a17b415dc53391abee5ef467854ad4ea8c1322cc04337e815710ba2de2a69447'
